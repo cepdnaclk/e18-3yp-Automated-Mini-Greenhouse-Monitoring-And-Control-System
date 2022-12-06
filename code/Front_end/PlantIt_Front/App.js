42a2c29fa28470386508f3cd394140617f760dc0
IshtaJayakody <e18149@eng.pdn.ac.lk>
2022-12-06 17:11:25 +0530

    ControlCard.js and ControlLayout.js designed

--- code/Front_end/PlantIt_Front/App.js
+++ code/Front_end/PlantIt_Front/App.js
@@ -2,13 +2,17 @@ import TextInput01 from './components/items/TextInput01';
 import LoginLayout from './components/layouts/LoginLayout';
 import SignInLayout from "./components/layouts/SignInLayout";
 import SignUpLayout from './components/layouts/SignUpLayout';
+import ControlLayout from './components/layouts/ControlLayout';
+import ControlCard from './components/items/ControlCard';
 export default function App() {
 
   return (
     //<LoginLayout></LoginLayout>
     //<TextInput01 text={"ishta"}></TextInput01>
     //<SignInLayout></SignInLayout>
-    <SignUpLayout></SignUpLayout>
+    //<SignUpLayout></SignUpLayout>
+    <ControlLayout></ControlLayout>
+    //<ControlCard text={"Soil moisture (gcm3)"}></ControlCard>
   );
 }
 
